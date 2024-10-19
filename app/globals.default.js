9b9cfee29d3bf015d677550dc4e05c2e5aea9515
MiningCryptoLive <celldr69@gmail.com>
2024-10-19 08:17:10 -0400

    Update globals.default.js

--- app/globals.default.js
+++ app/globals.default.js
@@ -4,9 +4,9 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
-		api_refresh_interval: 5000,
+		pool_name: "solopool.pro",
+		api_url : 'http://solopool.pro:4243',
+		api_refresh_interval: 30,
 		app_update_interval: 30*60000
 	};
 });
